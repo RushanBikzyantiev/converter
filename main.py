ce98032f5144d80fe5c523df4120aed7aa8ac442
RushanBikzyantiev <78953349+RushanBikzyantiev@users.noreply.github.com>
2021-10-28 21:32:50 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def convert_to_cm(system,unit,number):
+    if system == 'american':
+        if unit == 'inch':
+            result=round(number*2.54,2)
+    return(result)
