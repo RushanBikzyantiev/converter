ed42e2b007db387926922dba488af50bcfdba96c
Truong Son <tr.son.2112@gmail.com>
2021-10-30 01:25:57 +0300

    Add main function

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def convert_to_cm(system,unit,number):
             result = round(number * 17.78, 2)
         if unit=='fathom':
             result = round(number * 213.36, 2)
-    return(result)
+    return result
 
 def convert_from_cm(system, unit, number):
     if system == 'american':
@@ -37,4 +37,91 @@ def convert_from_cm(system, unit, number):
             result = round(number / 4.445, 2)
 
 
-    return result 
+    return result
+
+while True:
+    print("-----------------Меню-----------------")
+    print("[1] - Преобразовать из американского в старорусский")
+    print("[2] - Преобразовать из американского в сантиметр")
+    print("[3] - Преобразовать из старорусского в американский")
+    print("[4] - Преобразовать из старорусского в сантиметр")
+
+    choose = int(input("Введите ваш выбор: "))
+    print("--------------------------------------\n")\
+        
+    if choose == 1:
+        print("Выбрать исходную систему: ")
+        print("\t[1] - Inch")
+        print("\t[2] - Foot")
+        print("\t[3] - Mi")
+        source_system = int(input("Ваш выбор: "))
+        if source_system == 1:
+            source_system = "inch"
+        elif source_system == 2:
+            source_system = "foot"
+        elif source_system == 3:
+            source_system = "mi"
+        print("Выбрать целевую систему: ")
+        print("\t[1] - Verst")
+        print("\t[2] - Arshin")
+        print("\t[3] - Vershok")
+        target_system = int(input("Ваш выбор: "))
+        if target_system == 1:
+            target_system = 'verst'
+        elif target_system == 2:
+            target_system = 'arshin'
+        elif target_system == 3:
+            target_system = 'vershok'
+        
+        num = int(input("Введите число, которое вы хотите преобразовать: "))
+        print("Результат: " + str(convert_from_cm('oldrussian', target_system, convert_to_cm('american', source_system, num))))
+
+    elif choose == 2:
+        print("Выбрать исходную систему: ")
+        print("\t[1] - Inch")
+        print("\t[2] - Foot")
+        print("\t[3] - Mi")
+        source_system = int(input("Ваш выбор: "))
+        if source_system == 1:
+            source_system = "inch"
+        elif source_system == 2:
+            source_system = "foot"
+        elif source_system == 3:
+            source_system = "mi"
+        num = int(input("Введите число, которое вы хотите преобразовать: "))
+        print("Результат: " + str(convert_to_cm('american', source_system, num)))
+
+    elif choose == 3:
+        print("Выбрать исходную систему: ")
+        print("\t[1] - Локоть")
+        print("\t[2] - Пядь")
+        print("\t[3] - Вникать")
+        source_system = int(input("Ваш выбор: "))
+        if source_system == 1:
+            source_system = "Elbow"
+        elif source_system == 2:
+            source_system = "span"
+        elif source_system == 3:
+            source_system = "fathom"
+        print("Выбрать целевую систему: ")
+        print("\t[1] - Hand")
+        print("\t[2] - Yard")
+        print("\t[3] - Rod")
+        target_system = int(input("Ваш выбор: "))
+        if target_system == 1:
+            target_system = 'hand'
+        elif target_system == 2:
+            target_system = 'yard'
+        elif target_system == 3:
+            target_system = 'rod'
+        
+        num = int(input("Введите число, которое вы хотите преобразовать: "))
+        print("Результат: " + str(convert_from_cm('american', target_system, convert_to_cm('oldrussian', source_system, num))))
+    elif choose == 4:
+        print("Выбрать исходную систему: ")
+        print("\t[1] - Локоть")
+        print("\t[2] - Пядь")
+        print("\t[3] - Вникать")
+        source_system = int(input("Ваш выбор: "))
+        num = int(input("Введите число, которое вы хотите преобразовать: "))
+        print("Результат: " + str(convert_to_cm('oldrussian', source_system, num)))
